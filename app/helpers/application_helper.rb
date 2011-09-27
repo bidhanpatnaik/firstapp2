4fdfacb8f87c38d0340af559bc1f715dc99326c6
Bidhan Patnaik <bidhan@patnaiks.com>
2011-09-27 19:53:12 -0400

    initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+
+	def title
+		base_title = "Ruby on rail tutorial sample app"
+		if @title.nil?
+			base_title
+		else
+			"#{base_title} | #{@title}"
+		end 
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '19ca08acdeb66fa5811d8b8d4a725a430402a1c075f226462ebe2c6f461e2e407cefeecd419d93746792123985df2c92d33d9e00946ec241613a061c57d2ca51'
